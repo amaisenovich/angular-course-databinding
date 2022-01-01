cfcff4d7bf58746add153d8578db40c0a69fa5ff
Aliaksei Maisenovich <a.maisenovich@gmail.com>
2022-01-01 15:59:49 +0300

    removes redundant code

--- src/app/tabs/tab/tab.component.ts
+++ src/app/tabs/tab/tab.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab',
-  templateUrl: './tab.component.html',
-  styleUrls: ['./tab.component.css']
+  templateUrl: './tab.component.html'
 })
-export class TabComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class TabComponent {
 
 }
